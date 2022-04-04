d23edb1746718694eef10a5b39125e5d238cf99a
GuangZ96 <z251391809@126.com>
2022-04-04 21:23:25 +0800

    添加下拉刷新，配置ts按需引入

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,57 @@
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { 
+    defineComponent,
+    reactive,
+    toRefs, 
+} from 'vue'
+import { Button, PullRefresh, Toast } from 'vant';
 
 export default defineComponent({
+    components: {
+        [Button.name]: Button,
+        [PullRefresh.name]: PullRefresh,
+        [Toast.name]: Toast,
+    },
     setup() {
-        
+        const state = reactive({
+            count: 0, // 刷新次数
+            loading: false,
+        })
+
+        const onRefresh = () => {
+            setTimeout(() => {
+                Toast('刷新成功')
+                state.loading = false,
+                state.count++;
+            }, 3000)
+        }
+
+        return {
+            ...toRefs(state),
+
+            onRefresh,
+        }
     },
 })
 </script>
 
 <template>
     <div class="wrapper">
-        <!-- <van-button type="primary" plain>按钮</van-button> -->
-        hello!
+        <!-- 下拉刷新区域 -->
+        <van-pull-refresh
+            v-model="loading"
+            @refresh="onRefresh"
+        >
+            <p>刷新次数: {{ count }}</p>
+        </van-pull-refresh>
     </div>
 </template>
 
 <style lang="less" scoped>
-
+    .wrapper {
+        height: calc(100vh - 50px); /*no*/
+        .van-pull-refresh {
+            height: 100%;
+        }
+    }
 </style>
